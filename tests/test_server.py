815d1f4bfa98d7f6ad8e7be08d406df75944e0be
jkim10 <jyk2149@columbia.edu>
2020-11-11 21:04:22 -0500

    Removing outdated test and adding requirement

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,14 +1,6 @@
 import json
 
 
-# TEST SERVER CREATION
-def test_index(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
-    expected = {'hello': 'world'}
-    assert expected == json.loads(res.get_data(as_text=True))
-
-
 # Test SocketIO connection
 def test__client_connect(app, sclient):
     print(sclient)
