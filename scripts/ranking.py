82efea951d37ca4f861426a7ba69e19a92758554
jkim10 <jyk2149@columbia.edu>
2020-12-04 17:44:31 -0500

    firebase rehaul

--- scripts/ranking.py
+++
@@ -1,5 +0,0 @@
-def generateRanking(roomData, number_shown, anonymous=True):
-    mergedArray = []
-    for key in roomData:
-        mergedArray = mergedArray + roomData[key]
-    return max(set(mergedArray), key=mergedArray.count)
--- tests/test_server.py
+++
@@ -1,9 +0,0 @@
-
-# # TEST SERVER CREATION
-# def test_index(app, client):
-#     assert (True == True)
-
-
-# # Test SocketIO connection
-# def test__client_connect(app, sclient):
-#     assert (True == True)
